40a3aa2f335cc98fbe080b26062b73e734ec5a6a
Colcolat <jj.zapatabuenfil@gmail.com>
2026-05-23 03:33:19 -0600

    git commit -m "chore: structure setup with Package by Feature"

---
+++ src/main/java/colcolat/javasinoweb/JavasinoWebApplication.java
@@ -0,0 +1,13 @@
+package colcolat.javasinoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasinoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavasinoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/colcolat/javasinoweb/JavasinoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package colcolat.javasinoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavasinoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
